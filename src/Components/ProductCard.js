574f992a1e39ef33fbc6d228f7189710ff5174d2
javierbeltran425 <00019315@uca.edu.sv>
2021-11-23 20:09:54 -0600

    Correction in product card and pagination

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,13 +1,16 @@
 const ProductCard = ({ name, code, url, maker }) => {
+    console.log(url)
     return(
         <>
-            <a href={url} className="flex flex-col flex-wrap w-60 h-20 bg-white justify-around rounded-lg shadow-lg transform hover:scale-105 duration-500 m-5 cursor-pointer">
-                <div className="flex flex-row pl-2">
+            <a href={url} className="flex flex-col flex-wrap w-60 h-auto bg-white justify-around rounded-lg shadow-lg transform hover:scale-105 duration-500 m-5 p-2 cursor-pointer">
+                <div className="flex flex-row px-2 py-4">
                     <h2 className="font-bold">{name}</h2>
                 </div>
+                <hr/>
                 <div>
-                    <p className="font-bold pl-2">Codigo: {code}</p>
+                    <p className="pl-2">Codigo: {code}</p>
                 </div>
+                <hr/>
                 <div className="pl-2 text-red-600">
                     <p>
                         Marca: {maker}
--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -1,76 +1,121 @@
-import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-import { useState } from 'react'
-import { useAxiosPost } from '../Hooks/useAxiosPost'
+import { useState } from "react";
+import { useAxiosPost } from "../Hooks/useAxiosPost";
 
-import Header from '../Components/Header'
-import ProductCard from '../Components/ProductCard'
-import Footer from '../Components/Footer'
+import Header from "../Components/Header";
+import ProductCard from "../Components/ProductCard";
+import Footer from "../Components/Footer";
 
 const Products = () => {
-    const [ search, setSearch ] = useState(null)
-    const [page, setPage] = useState(1)
-    const [ searchContent, setSearchContent ] = useState(null)
+  const [search, setSearch] = useState(null);
+  const [page, setPage] = useState(1);
+  const [searchContent, setSearchContent] = useState(null);
 
-    var category = localStorage.getItem('category')
-    let auxData = {
-        category: category
+  var category = localStorage.getItem("category");
+  let auxData = {
+    category: category,
+  };
+
+  let petitionFilter = useAxiosPost(
+    process.env.REACT_APP_API_URL + "products/product-category?page=" + page,
+    auxData
+  );
+
+  let content = null;
+
+  const onChange = async (e) => {
+    e.persist();
+
+    await setSearch(e.target.value.toLowerCase());
+
+    filterElements();
+  };
+
+  const filterElements = () => {
+    var searchE = petitionFilter.response.products.filter((item) => {
+      if (
+        item.name.toLowerCase().includes(search) ||
+        item.productID.toLowerCase().includes(search) ||
+        item.maker.toLowerCase().includes(search)
+      )
+        return item;
+    });
+    setSearchContent(searchE);
+  };
+
+  if (category) {
+    if (searchContent == null || search.length < 2) {
+      console.log(search);
+      if (petitionFilter.response != null) {
+        content = petitionFilter.response.products.map((e) => (
+          <ProductCard
+            name={`${e.name}`}
+            code={e.productID}
+            url={e.url}
+            maker={e.maker}
+          />
+        ));
+      }
+    } else {
+      content = searchContent.map((e) => (
+        <ProductCard
+          name={`${e.name}`}
+          code={e.productID}
+          url={e.url}
+          maker={e.maker}
+        />
+      ));
     }
+  }
 
-    let petitionFilter = useAxiosPost(process.env.REACT_APP_API_URL + 'products/product-category?page=' + page, auxData)
+  return (
+    <>
+      <Header className="bg-gray-900" />
+      <div className="flex flex-col justify-between w-full min-h-screen bg-gray-400">
+        <div className="flex flex-row w-full justify-center items-center pt-20">
+          <input
+            value={search}
+            onChange={onChange}
+            type="text"
+            placeholder="Buscar..."
+            className="my-3 border rounded shadow-lg text-gray-600 px-2 focus:border-purple-700 focus:ring-1 focus:ring-purple-700 outline-none w-3/4"
+          />
+          <Icon icon={faSearch} className="mx-3" />
+        </div>
 
-    let content = null
+        <div className="flex flex-row flex-wrap justify-center w-full">
+          {content}
+        </div>
 
-    const onChange = async e => {
-        e.persist();
+        <div className="bottom-0 w-full text-center">
+          <Icon
+            icon={faArrowLeft}
+            className="mx-20 my-2 hover:scale-110 transform duration-500 cursor-pointer"
+            onClick={(e) => {
+              e.preventDefault();
+              if (page > 1) {
+                setPage(page - 1);
+              }
+            }}
+          />
+          <Icon
+            icon={faArrowRight}
+            className="mx-20 my-2 hover:scale-110 transform duration-500 cursor-pointer"
+            onClick={(e) => {
+              if (page <= petitionFilter.response.totalPages - 1) {
+                setPage(page + 1);
+              }
+            }}
+          />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-        await setSearch(e.target.value.toLowerCase())
-
-        filterElements()
-    }
-
-    const filterElements = () => {
-        
-        var searchE = petitionFilter.response.products.filter(item =>{
-            if(item.name.toLowerCase().includes(search) || item.productID.toLowerCase().includes(search) || item.maker.toLowerCase().includes(search))
-                return item
-        });
-        setSearchContent(searchE)
-    }
-
-    if(category){
-        if(searchContent == null || search.length < 2){
-            console.log(search)
-            if(petitionFilter.response != null){
-                content = petitionFilter.response.products.map(e => 
-                    <ProductCard name={`${e.name}`} code={e.productID} url={e.url} maker={e.maker} />
-                )
-            }
-        }else{
-            content = searchContent.map(e => 
-                <ProductCard name={`${e.name}`} code={e.productID} url={e.url} maker={e.maker} />
-            )
-        }
-    }
-
-    return(
-        <>
-            <Header className="bg-gray-900"/>
-            <div className="w-full min-h-screen bg-gray-400">
-                <div className="flex flex-row w-full justify-center items-center pt-20">
-                    <input value={search} onChange={onChange} type="text" placeholder="Buscar..." className="my-3 border rounded shadow-lg text-gray-600 px-2 focus:border-purple-700 focus:ring-1 focus:ring-purple-700 outline-none w-3/4"/>
-                    <Icon icon={faSearch} className="mx-3"/>
-                </div>
-
-                <div className="flex flex-row flex-wrap justify-center w-full">
-                    {content}
-                </div>
-
-            </div>
-            <Footer />
-        </>
-    )
-}
-
-export default Products
+export default Products;
